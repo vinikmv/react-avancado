788edb4c11650f9d93e9ce5c50817d5c7c8bc5ee
viniciuskmv <viniciuskmv@gmail.com>
2021-09-28 22:57:38 -0300

    Criação do GameCard

---
+++ src/components/GameCard/stories.tsx
@@ -0,0 +1,36 @@
+import { Story, Meta } from '@storybook/react'
+import GameCard, { GameCardProps } from '.'
+
+export default {
+  title: 'GameCard',
+  component: GameCard,
+  args: {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 200,00'
+  },
+  argTypes: {
+    onFav: { action: 'clicked' },
+    ribbon: { type: 'string' }
+  }
+} as Meta
+
+export const Default: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
+
+export const WithRibbon: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
+
+WithRibbon.args = {
+  ribbon: '20% OFF',
+  ribbonSize: 'small',
+  ribbonColor: 'primary'
+}
---
+++ src/components/GameCard/test.tsx
@@ -0,0 +1,83 @@
+import { fireEvent, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameCard from '.'
+
+const props = {
+  title: 'Population Zero',
+  developer: 'Rockstar Games',
+  img: '/img/red-dead-img.jpg',
+  price: 'R$ 235,00'
+}
+
+describe('<GameCard />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<GameCard {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: props.developer })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+
+    expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render price in label', () => {
+    renderWithTheme(<GameCard {...props} />)
+
+    const price = screen.getByText(props.price)
+
+    expect(price).not.toHaveStyle({ textDecoration: 'line-through' })
+
+    expect(price).toHaveStyle({ backgroundColor: '#3CD3C1' })
+  })
+
+  it('should render a line-through in price when promotional', () => {
+    renderWithTheme(<GameCard {...props} promotionalPrice="R$ 15,00" />)
+    const price = screen.getByText(props.price)
+    expect(price).toHaveStyle({ textDecoration: 'line-through' })
+
+    const promotionalPrice = screen.getByText('R$ 15,00')
+    expect(promotionalPrice).not.toHaveStyle({ textDecoration: 'line-through' })
+  })
+
+  it('should render filled Favorite icon when favorite is true', () => {
+    renderWithTheme(<GameCard {...props} favorite />)
+
+    expect(screen.getByLabelText(/remove from wishlist/i)).toBeInTheDocument()
+  })
+
+  it('should render call onFav method when favorite is clicked', () => {
+    const onFav = jest.fn()
+    renderWithTheme(<GameCard {...props} favorite onFav={onFav} />)
+
+    fireEvent.click(screen.getAllByRole('button')[0])
+    expect(onFav).toBeCalled()
+  })
+
+  it('should render Ribbon', () => {
+    renderWithTheme(
+      <GameCard
+        {...props}
+        ribbon="My Ribbon"
+        ribbonColor="secondary"
+        ribbonSize="small"
+      />
+    )
+    const ribbon = screen.getByText(/my ribbon/i)
+
+    expect(ribbon).toHaveStyle({ backgroundColor: '#3CD3C1' })
+    expect(ribbon).toHaveStyle({ height: '2.6rem', fontSize: '1.2rem' })
+    expect(ribbon).toBeInTheDocument()
+  })
+})
