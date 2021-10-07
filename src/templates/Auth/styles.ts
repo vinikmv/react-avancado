0697640d960608605205383c9ba7edd0345dc34c
viniciuskmv <viniciuskmv@gmail.com>
2021-10-07 20:36:26 -0300

    Corrigindo bug do SVG e adicionando opção huge para o Heading

--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -1,12 +1,17 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 import * as HeadingStyles from 'components/Heading/styles'
 import * as LogoStyles from 'components/Logo/styles'
 
 export const Wrapper = styled.main`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr;
   height: 100vh;
+
+  ${media.greaterThan('medium')`
+    grid-template-columns: 1fr 1fr;
+  `}
 `
 
 export const BannerBlock = styled.div`
@@ -16,15 +21,19 @@ export const BannerBlock = styled.div`
     background-size: cover;
     background-position: center center;
     padding: ${theme.spacings.xxlarge} ${theme.spacings.xxlarge}
-      ${theme.spacings.xlarge};
+      ${theme.spacings.large};
+
+    ${media.lessThan('medium')`
+      display: none;
+    `}
 
     &:after {
       content: '';
       position: absolute;
       top: 0;
       bottom: 0;
-      right: 0;
       left: 0;
+      right: 0;
       background: ${theme.colors.black};
       opacity: 0.85;
     }
@@ -34,13 +43,12 @@ export const BannerBlock = styled.div`
 export const BannerContent = styled.div`
   ${({ theme }) => css`
     color: ${theme.colors.white};
-    position: relative;
-    z-index: ${theme.layers.base};
-
     display: grid;
     grid-template-columns: 1fr;
     justify-content: space-between;
     height: 100%;
+    position: relative;
+    z-index: ${theme.layers.base};
   `}
 `
 
@@ -76,9 +84,15 @@ export const Content = styled.div`
 export const ContentWrapper = styled.div`
   ${({ theme }) => css`
     width: 30rem;
+
+    ${media.greaterThan('medium')`
+      width: 36rem;
+    `}
+
     ${LogoStyles.Wrapper} {
       margin: 0 auto ${theme.spacings.xxlarge};
     }
+
     ${HeadingStyles.Wrapper} {
       margin-bottom: ${theme.spacings.medium};
     }
