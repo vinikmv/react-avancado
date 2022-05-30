9d2fa786e1cafc44eba8b0f751362674fad0c9ce
viniciuskmv <viniciuskmv@gmail.com>
2022-05-30 13:08:39 -0300

    Create Grid and Divider Components

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,3 +1,4 @@
+import { Divider } from 'components/Divider'
 import Gallery, { GalleryImageProps } from 'components/Gallery'
 import { GameCardProps } from 'components/GameCard'
 import GameDetails, { GameDetailsProps } from 'components/GameDetails'
@@ -46,6 +47,7 @@ const Game = ({
 
       <S.SectionGameDetails>
         <GameDetails {...details} />
+        <Divider />
       </S.SectionGameDetails>
 
       <Showcase
