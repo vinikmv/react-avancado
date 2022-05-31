6e5831b65fc879e7a5dc5d7f0fb27a2c0ba44ff2
viniciuskmv <viniciuskmv@gmail.com>
2022-05-31 13:41:08 -0300

    create PaymentOptions component, change button to include disabled option and add tests

---
+++ src/components/PaymentOptions/mock.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    flag: 'mastercard',
+    img: '/img/cards/mastercard.png',
+    number: '**** **** **** 4326'
+  },
+  {
+    flag: 'visa',
+    img: '/img/cards/visa.png',
+    number: '**** **** **** 4325'
+  }
+]
