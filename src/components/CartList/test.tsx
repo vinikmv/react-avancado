677b64f093542675b4bdca264e0433a21317c80d
viniciuskmv <viniciuskmv@gmail.com>
2022-06-03 16:19:54 -0300

    modify CartList to accept empty cart

--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -16,4 +16,17 @@ describe('<CartList />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render the cart list with button', () => {
+    renderWithTheme(<CartList items={mockItems} total="R$ 330" hasButton />)
+
+    expect(screen.getByText(/buy it now/i)).toBeInTheDocument()
+  })
+
+  it('should render empty if there are no games', () => {
+    renderWithTheme(<CartList />)
+
+    expect(screen.getByText(/your cart is empty/i)).toBeInTheDocument()
+    expect(screen.queryByText(/total/i)).not.toBeInTheDocument()
+  })
 })
