37f3481df6eb87fa74dc6be09f1a60240aee57d2
viniciuskmv <viniciuskmv@gmail.com>
2022-06-14 16:00:30 -0300

    add methods to total and quantity in useCart

--- src/hooks/use-cart/index.tsx
+++ src/hooks/use-cart/index.tsx
@@ -15,9 +15,15 @@ type CartItem = {
 
 export type CartContextData = {
   items: CartItem[]
+  quantity: number
+  total: string
 }
 
-export const CartContextDefaultValues = { items: [] }
+export const CartContextDefaultValues = {
+  items: [],
+  quantity: 0,
+  total: '$0.00'
+}
 
 export const CartContext = createContext<CartContextData>(
   CartContextDefaultValues
@@ -43,10 +49,16 @@ const CartProvider = ({ children }: CartProviderProps) => {
     }
   })
 
+  const total = data?.games.reduce((acc, game) => {
+    return acc + game.price
+  }, 0)
+
   return (
     <CartContext.Provider
       value={{
-        items: cartMapper(data?.games)
+        items: cartMapper(data?.games),
+        quantity: cartItems.length,
+        total: formatPrice(total || 0)
       }}
     >
       {children}
