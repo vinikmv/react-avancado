e02e51517b5e0413ccf24283948d0afee94f240a
viniciuskmv <viniciuskmv@gmail.com>
2022-06-15 13:54:12 -0300

    edit GameInfo to use Cartbutton

--- src/components/GameInfo/test.tsx
+++ src/components/GameInfo/test.tsx
@@ -3,14 +3,14 @@ import { render, screen } from 'utils/test-utils'
 import GameInfo from '.'
 
 const props = {
+  id: '1',
   title: 'My Game Title',
   description: 'Game Description',
   price: 210
 }
 describe('<GameInfo />', () => {
   it('should render game informations', () => {
-    const { container } = 
-render(<GameInfo {...props} />)
+    const { container } = render(<GameInfo {...props} />)
 
     expect(
       screen.getByRole('heading', { name: /my game title/i })
@@ -22,8 +22,7 @@ render(<GameInfo {...props} />)
   })
 
   it('should render buttons', () => {
-    
-render(<GameInfo {...props} />)
+    render(<GameInfo {...props} />)
 
     expect(
       screen.getByRole('button', { name: /add to cart/i })
