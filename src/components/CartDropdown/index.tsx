93b64bfdc537c8b279f423a2aa2be422ccb9f6d4
viniciuskmv <viniciuskmv@gmail.com>
2022-06-15 13:06:21 -0300

    edit CartDropdown to use useCart hook

--- src/components/CartDropdown/index.tsx
+++ src/components/CartDropdown/index.tsx
@@ -1,19 +1,11 @@
 import CartIcon from 'components/CartIcon'
 import CartList from 'components/CartList'
 import Dropdown from 'components/Dropdown'
-import { GameItemProps } from 'components/GameItem'
 import * as S from './styles'
 
-export type CartDropdownProps = {
-  items?: GameItemProps[]
-  total?: string
-}
-
-const CartDropdown = ({ items, total }: CartDropdownProps) => (
+const CartDropdown = () => (
   <S.Wrapper>
-    <Dropdown title={<CartIcon />}>
-      {<CartList items={items} total={total} hasButton />}
-    </Dropdown>
+    <Dropdown title={<CartIcon />}>{<CartList hasButton />}</Dropdown>
   </S.Wrapper>
 )
 
--- src/components/CartDropdown/test.tsx
+++ src/components/CartDropdown/test.tsx
@@ -3,21 +3,26 @@ import { render, screen } from 'utils/test-utils'
 import items from 'components/CartList/mock'
 
 import CartDropdown from '.'
+import { CartContextDefaultValues } from 'hooks/use-cart'
 
 describe('<CartDropdown />', () => {
-  it('should render <CartIcon /> and its badge', () => {
-    
-render(<CartDropdown items={items} total="R$ 200" />)
+  beforeEach(() => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items,
+      quantity: items.length,
+      total: 'R$ 330,00'
+    }
 
+    render(<CartDropdown />, { cartProviderProps })
+  })
+  it('should render <CartIcon /> and its badge', () => {
     expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
     expect(screen.getByText(`${items.length}`)).toBeInTheDocument()
   })
 
   it('should render Dropdown content with cart items and total', () => {
-    
-render(<CartDropdown items={items} total="R$ 200" />)
-
-    expect(screen.getByText('R$ 200')).toBeInTheDocument()
+    expect(screen.getByText('R$ 330,00')).toBeInTheDocument()
     expect(screen.getByText(`${items[0].title}`)).toBeInTheDocument()
   })
 })
