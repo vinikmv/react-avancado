3e7c0554b5176d4990b0c486a2163216379aa983
viniciuskmv <viniciuskmv@gmail.com>
2022-06-15 12:55:51 -0300

    edit CartList to use useCart hook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -25,7 +25,7 @@ export const decorators = [
       <CartContext.Provider
         value={{
           ...CartContextDefaultValues,
-          ...(context?.args?.CartContextValue || {}),
+          ...(context?.args?.cartContextValue || {}),
           ...context.args
         }}
       >
--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -1,14 +1,17 @@
+import { CartContextDefaultValues } from 'hooks/use-cart'
 import { render, screen } from 'utils/test-utils'
 
 import CartList from '.'
-import mockItems from './mock'
+import items from './mock'
 
 describe('<CartList />', () => {
   it('should render the cart list', () => {
-    const { container } = 
-render(
-      <CartList items={mockItems} total="R$ 330" />
-    )
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items,
+      total: 'R$ 330'
+    }
+    const { container } = render(<CartList />, { cartProviderProps })
 
     expect(screen.getAllByRole('heading')).toHaveLength(2)
     expect(screen.getByText('R$ 330')).toHaveStyle({ color: '#F231A5' })
@@ -18,15 +21,17 @@ render(
   })
 
   it('should render the cart list with button', () => {
-    
-render(<CartList items={mockItems} total="R$ 330" hasButton />)
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items
+    }
+    render(<CartList hasButton />, { cartProviderProps })
 
     expect(screen.getByText(/buy it now/i)).toBeInTheDocument()
   })
 
   it('should render empty if there are no games', () => {
-    
-render(<CartList />)
+    render(<CartList />)
 
     expect(screen.getByText(/your cart is empty/i)).toBeInTheDocument()
     expect(screen.queryByText(/total/i)).not.toBeInTheDocument()
