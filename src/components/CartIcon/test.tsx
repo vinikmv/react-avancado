7f6dd199580db3b265fb20cfffebeeee2be49e77
viniciuskmv <viniciuskmv@gmail.com>
2022-06-15 11:20:58 -0300

    update cartIcon and cartDropdown to work on storybook

--- src/components/CartIcon/test.tsx
+++ src/components/CartIcon/test.tsx
@@ -1,29 +1,22 @@
+import { CartContextDefaultValues } from 'hooks/use-cart'
 import { render, screen } from 'utils/test-utils'
 
 import CartIcon from '.'
 
 describe('<CartIcon />', () => {
   it('should render without badge', () => {
-    
-render(<CartIcon />)
+    render(<CartIcon />)
 
     expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
     expect(screen.queryByLabelText(/cart items/i)).not.toBeInTheDocument()
   })
 
   it('should render with badge', () => {
-    
-render(<CartIcon quantity={2} />)
+    render(<CartIcon />, {
+      cartProviderProps: { ...CartContextDefaultValues, quantity: 2 }
+    })
 
     expect(screen.queryByLabelText(/cart items/i)).toBeInTheDocument()
     expect(screen.getByText(/2/)).toBeInTheDocument()
-  })
-
-  it('should render with badge only if has positive number', () => {
-    
-render(<CartIcon quantity={-1} />)
-
-    expect(screen.queryByLabelText(/cart items/i)).not.toBeInTheDocument()
-    expect(screen.queryByText(/-1/)).not.toBeInTheDocument()
   })
 })
