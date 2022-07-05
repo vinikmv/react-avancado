1261ce1076aefbec4ba272fde9e5ca45451b9469
viniciuskmv <viniciuskmv@gmail.com>
2022-07-05 14:18:49 -0300

    create useWishlist logic with TDD

--- src/hooks/use-wishlist/test.tsx
+++ src/hooks/use-wishlist/test.tsx
@@ -29,4 +29,22 @@ describe('useWishlist', () => {
       wishlistItems[1]
     ])
   })
+
+  it('should check if game is in the wishlist', async () => {
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <MockedProvider mocks={[wishlistMock]}>
+        <WishlistProvider>{children}</WishlistProvider>
+      </MockedProvider>
+    )
+
+    const { result, waitForNextUpdate } = renderHook(() => useWishlist(), {
+      wrapper
+    })
+
+    await waitForNextUpdate()
+
+    expect(result.current.isInWishlist('1')).toBe(true)
+    expect(result.current.isInWishlist('2')).toBe(true)
+    expect(result.current.isInWishlist('3')).toBe(false)
+  })
 })
