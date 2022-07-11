5bd234a8dbeb484468b0b3f7836e1cf49b8c6787
viniciuskmv <viniciuskmv@gmail.com>
2022-07-11 14:27:04 -0300

    add addToWishlist and removeFromWishlist to component with tests

--- src/components/WishlistButton/test.tsx
+++ src/components/WishlistButton/test.tsx
@@ -1,5 +1,7 @@
-import { render, screen } from 'utils/test-utils'
+import userEvent from '@testing-library/user-event'
 import { WishlistContextDefaultValues } from 'hooks/use-wishlist'
+import { act, render, screen } from 'utils/test-utils'
+
 import WishlistButton from '.'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -56,12 +58,46 @@ describe('<WishlistButton />', () => {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const useSession = jest.spyOn(require('next-auth/client'), 'useSession')
     useSession.mockImplementationOnce(() => [null])
+
     const wishlistProviderProps = {
       ...WishlistContextDefaultValues,
       isInWishlist: () => true
     }
 
     render(<WishlistButton id="1" hasText />, { wishlistProviderProps })
+
     expect(screen.queryByText(/remove from wishlist/i)).not.toBeInTheDocument()
+  })
+
+  it('should add to wishlist', async () => {
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      isInWishlist: () => false,
+      addToWishlist: jest.fn()
+    }
+
+    render(<WishlistButton id="1" hasText />, { wishlistProviderProps })
+
+    act(() => {
+      userEvent.click(screen.getByText(/add to wishlist/i))
+    })
+
+    expect(wishlistProviderProps.addToWishlist).toHaveBeenCalledWith('1')
+  })
+
+  it('should remove from wishlist', async () => {
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      isInWishlist: () => true,
+      removeFromWishlist: jest.fn()
+    }
+
+    render(<WishlistButton id="1" hasText />, { wishlistProviderProps })
+
+    act(() => {
+      userEvent.click(screen.getByText(/remove from wishlist/i))
+    })
+
+    expect(wishlistProviderProps.removeFromWishlist).toHaveBeenCalledWith('1')
   })
 })
