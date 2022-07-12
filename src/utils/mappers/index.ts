840e9001780ec42b4838e98bf684d4cc62a9a69c
viniciuskmv <viniciuskmv@gmail.com>
2022-07-12 15:43:40 -0300

    use Next Image

--- src/utils/mappers/index.ts
+++ src/utils/mappers/index.ts
@@ -10,7 +10,7 @@ import formatPrice from 'utils/format-price'
 
 export const bannerMapper = (banners: QueryHome_banners[]) => {
   return banners.map((banner) => ({
-    img: `http://localhost:1337${banner.image?.url}`,
+    img: `${banner.image?.url}`,
     title: banner.title,
     subtitle: banner.subtitle,
     buttonLabel: banner.button?.label,
@@ -32,7 +32,7 @@ export const gamesMapper = (
         title: game.name,
         slug: game.slug,
         developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover?.url}`,
+        img: `${game.cover?.url}`,
         price: game.price
       }))
     : []
@@ -45,8 +45,8 @@ export const highlightMapper = (
     ? {
         title: highlight.title,
         subtitle: highlight.subtitle,
-        backgroundImage: `http://localhost:1337${highlight.background?.url}`,
-        floatImage: `http://localhost:1337${highlight.floatImage?.url}`,
+        backgroundImage: `${highlight.background?.url}`,
+        floatImage: `${highlight.floatImage?.url}`,
         buttonLabel: highlight.buttonLabel,
         buttonLink: highlight.buttonLink,
         alignment: highlight.alignment
@@ -58,7 +58,7 @@ export const cartMapper = (games: QueryGames_games[] | undefined) => {
   return games
     ? games.map((game) => ({
         id: game.id,
-        img: `http://localhost:1337${game.cover?.url}`,
+        img: `${game.cover?.url}`,
         title: game.name,
         price: formatPrice(game.price)
       }))
@@ -86,7 +86,7 @@ export const ordersMapper = (orders: QueryOrders_orders[] | undefined) => {
           title: game.name,
           downloadLink:
             'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
-          img: `http://localhost:1337${game.cover?.url}`,
+          img: `${game.cover?.url}`,
           price: formatPrice(game.price)
         }))
       }))
