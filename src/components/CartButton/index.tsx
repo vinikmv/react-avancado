ce5a6d800e41dd8921ddf0405fb9c0bff30c37c7
viniciuskmv <viniciuskmv@gmail.com>
2022-07-12 18:37:56 -0300

    add Google Analytics

--- src/components/CartButton/index.tsx
+++ src/components/CartButton/index.tsx
@@ -2,6 +2,7 @@ import {
   AddShoppingCart,
   RemoveShoppingCart
 } from '@styled-icons/material-outlined'
+
 import Button, { ButtonProps } from 'components/Button'
 import { useCart } from 'hooks/use-cart'
 
@@ -17,17 +18,13 @@ const CartButton = ({
 }: CartButtonProps) => {
   const { isInCart, addToCart, removeFromCart } = useCart()
   const ButtonText = isInCart(id) ? 'Remove from cart' : 'Add to cart'
+
   return (
     <Button
-      icon={
-        isInCart(id) ? (
-          <RemoveShoppingCart aria-label="Remove from cart" />
-        ) : (
-          <AddShoppingCart aria-label="Add to cart" />
-        )
-      }
+      icon={isInCart(id) ? <RemoveShoppingCart /> : <AddShoppingCart />}
       size={size}
       onClick={() => (isInCart(id) ? removeFromCart(id) : addToCart(id))}
+      aria-label={ButtonText}
     >
       {hasText && ButtonText}
     </Button>
