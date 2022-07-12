5a8104acdf2aa834cda15232d42eadbe58330315
viniciuskmv <viniciuskmv@gmail.com>
2022-07-12 11:54:33 -0300

    listing orders

--- src/pages/profile/orders.tsx
+++ src/pages/profile/orders.tsx
@@ -1,7 +1,13 @@
 import OrdersList, { OrdersListProps } from 'components/OrdersList'
-import ordersMock from 'components/OrdersList/mock'
+import {
+  QueryOrders,
+  QueryOrdersVariables
+} from 'graphql/generated/QueryOrders'
+import { QUERY_ORDERS } from 'graphql/queries/orders'
 import { GetServerSidePropsContext } from 'next'
 import Profile from 'templates/Profile'
+import { initializeApollo } from 'utils/apollo'
+import { ordersMapper } from 'utils/mappers'
 import protectedRoutes from 'utils/protected-routes'
 
 export default function ProfileOrders({ items }: OrdersListProps) {
@@ -14,10 +20,18 @@ export default function ProfileOrders({ items }: OrdersListProps) {
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context)
+  const apolloClient = initializeApollo(null, session)
+
+  const { data } = await apolloClient.query<QueryOrders, QueryOrdersVariables>({
+    query: QUERY_ORDERS,
+    variables: {
+      identifier: session?.id
+    }
+  })
   return {
     props: {
-      session,
-      items: ordersMock
+      items: ordersMapper(data.orders),
+      session
     }
   }
 }
