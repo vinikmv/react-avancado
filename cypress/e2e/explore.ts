441f691e2c11f629dbd3620feb030c5cf5260216
viniciuskmv <viniciuskmv@gmail.com>
2022-07-16 17:48:13 -0300

    create test for price order

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -29,4 +29,19 @@ describe('Explore Page', () => {
     cy.getByDataCy('game-card').should('have.length', 30)
 
   });
+
+  it('should order by price', () => {
+    cy.findByText(/lowest to highest/i).click()
+
+    cy.location('href').should('contain', 'sort=price%3Aasc')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.findByText('FREE').should('exist')
+    })
+
+    cy.findByText(/highest to lowest/i).click()
+    cy.location('href').should('contain', 'sort=price%3Adesc')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeGreaterThan(0)
+    })
+  });
 })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,4 @@
-import '@testing-library/cypress/add-commands';
+import '@testing-library/cypress/add-commands'
 
 Cypress.Commands.add('google', () => cy.visit('https://google.com'))
 
@@ -8,29 +8,26 @@ Cypress.Commands.add('getByDataCy', (selector, ...args) => {
 
 Cypress.Commands.add('shouldRenderBanner', () => {
   cy.get('.slick-slider').within(() => {
-    cy.findByRole('heading', {name: /cyberpunk 2077/i})
-    cy.findByRole('link', {name: /buy now/i})
+    cy.findByRole('heading', { name: /cyberpunk 2077/i })
+    cy.findByRole('link', { name: /buy now/i })
 
     cy.get('.slick-dots > :nth-child(2) > button').click()
     cy.wait(500)
 
-    cy.findByRole('heading', {name: /horizon zero dawn/i})
-    cy.findByRole('link', {name: /buy now/i})
+    cy.findByRole('heading', { name: /horizon zero dawn/i })
+    cy.findByRole('link', { name: /buy now/i })
 
-    
     cy.get('.slick-dots > :nth-child(3) > button').click()
     cy.wait(500)
 
-    
-    cy.findByRole('heading', {name: /huge promotion/i})
-    cy.findByRole('link', {name: /browse games/i})
+    cy.findByRole('heading', { name: /huge promotion/i })
+    cy.findByRole('link', { name: /browse games/i })
   })
 })
 
-
-Cypress.Commands.add('shouldRenderShowcase', ({ name, highlight = false}) => {
+Cypress.Commands.add('shouldRenderShowcase', ({ name, highlight = false }) => {
   cy.getByDataCy(`"${name}"`).within(() => {
-    cy.findByRole('heading', {name}).should('exist')
+    cy.findByRole('heading', { name }).should('exist')
 
     cy.getByDataCy('highlight').should(highlight ? 'exist' : 'not.exist')
 
@@ -48,4 +45,20 @@ Cypress.Commands.add('getFields', (fields) => {
   fields.map(({ label }) => {
     cy.findByText(label).should('exist')
   })
+})
+
+Cypress.Commands.add('shouldBeGreaterThan', (value) => {
+  cy.findByText(/^\$\d+(\.\d{1,2})?/)
+    .invoke('text')
+    .then(($el) => $el.replace('$', ''))
+    .then(parseFloat)
+    .should('be.gt', value)
+})
+
+Cypress.Commands.add('shouldBeLessThan', (value) => {
+  cy.findByText(/^\$\d+(\.\d{1,2})?/)
+    .invoke('text')
+    .then(($el) => $el.replace('$', ''))
+    .then(parseFloat)
+    .should('be.lt', value)
 })
