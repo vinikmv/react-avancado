da14a915a8bad36989ad8faab0fa950864e612b5
viniciuskmv <viniciuskmv@gmail.com>
2022-07-18 13:48:51 -0300

    creating e2e test for reset password success case

--- cypress/e2e/reset-password.ts
+++ cypress/e2e/reset-password.ts
@@ -6,9 +6,11 @@ describe('Reset Password', () => {
 
     cy.findAllByPlaceholderText(/^password/i).type('123')
     cy.findAllByPlaceholderText(/confirm password/i).type('321')
-    cy.findByRole('button', {name: /reset password/i}).click()
+    cy.findByRole('button', { name: /reset password/i }).click()
 
-    cy.findByText(/confirm password does not match with password/i).should('exist')
+    cy.findByText(/confirm password does not match with password/i).should(
+      'exist'
+    )
   })
 
   it('should show error if provided code is invalid', () => {
@@ -25,8 +27,39 @@ describe('Reset Password', () => {
     cy.visit('/reset-password?code=invalidcode')
     cy.findAllByPlaceholderText(/^password/i).type('123456')
     cy.findAllByPlaceholderText(/confirm password/i).type('123456')
-    cy.findByRole('button', {name: /reset password/i}).click()
+    cy.findByRole('button', { name: /reset password/i }).click()
 
     cy.findByText(/incorrect code provided./i).should('exist')
-  });
+  })
+
+  it('should fill the input and redirect to the home page with the user signed in', () => {
+    cy.intercept('POST', '**/auth/reset-password', {
+      statusCode: 200,
+      body: { user: { email: 'cypress@email.com' } }
+    })
+
+    cy.intercept('POST', '**/auth/callback/credentials*', {
+      statusCode: 200,
+      body: { user: { email: 'cypress@email.com' } }
+    })
+
+    cy.intercept('POST', '**/auth/callback/credentials*', {
+      statusCode: 200,
+      body: { user: { email: 'cypress@email.com' } }
+    })
+
+    cy.intercept('GET', '**/auth/session*', {
+      statusCode: 200,
+      body: { user: { name: 'cypress', email: 'cypress@email.com' } }
+    })
+
+    cy.visit('/reset-password?code=valid-token')
+    cy.findAllByPlaceholderText(/^password/i).type('123456')
+    cy.findAllByPlaceholderText(/confirm password/i).type('123456')
+    cy.findByRole('button', { name: /reset password/i }).click()
+
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+
+    cy.findByText('cypress').should('exist')
+  })
 })
